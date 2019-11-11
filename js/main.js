335a94e908d7cef1187d46bba7a2289b17e08399
Graeme Stewart <gi_stewart@yahoo.com>
2019-11-11 17:16:19 -0500

    adds and deletes images, adds resume, updates README file

--- js/main.js
+++ js/main.js
@@ -1,49 +1,42 @@
 $(document).ready(function() {
-  $('.slider').slider({
-    interval: 2000
-  });
-  $('.button-collapse').sideNav();
+    $(window).scrollTop(0);
+    $('.slider').slider({
+        interval: 2000
+    });
+    $('.button-collapse').sideNav();
 });
 $('.modal').modal();
 $('.parallax').parallax();
 $('.scrollspy').scrollSpy({
-  scrollOffset: 0
+    scrollOffset: 0
 });
-var options = [
-  {
-    selector: '#staggered-list1',
-    offset: 100,
-    callback: function(el) {
-      Materialize.fadeInImage($(el));
+var options = [{
+        selector: '#staggered-list1',
+        offset: 100,
+        callback: function(el) {
+            Materialize.fadeInImage($(el));
+        }
+    },
+    {
+        selector: '#staggered-list2',
+        offset: 100,
+        callback: function(el) {
+            Materialize.fadeInImage($(el));
+        }
+    },
+    {
+        selector: '#about',
+        offset: 200,
+        callback: function(el) {
+            Materialize.fadeInImage($(el));
+        }
+    },
+    {
+        selector: '#contacts',
+        offset: 200,
+        callback: function(el) {
+            Materialize.fadeInImage($(el));
+        }
     }
-  },
-  {
-    selector: '#staggered-list2',
-    offset: 100,
-    callback: function(el) {
-      Materialize.fadeInImage($(el));
-    }
-  },
-  {
-    selector: '#staggered-list3',
-    offset: 100,
-    callback: function(el) {
-      Materialize.fadeInImage($(el));
-    }
-  },
-  {
-    selector: '#about',
-    offset: 200,
-    callback: function(el) {
-      Materialize.fadeInImage($(el));
-    }
-  },
-  {
-    selector: '#awards',
-    offset: 200,
-    callback: function(el) {
-      Materialize.fadeInImage($(el));
-    }
-  }
 ];
 Materialize.scrollFire(options);
